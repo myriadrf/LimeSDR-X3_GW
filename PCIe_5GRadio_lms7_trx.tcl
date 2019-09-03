0bee68ee1c1a9e6e57b22c64778da24dd9d3beb1
vytautasb <v.buitvydas@limemicro.com>
2019-09-03 11:31:26 +0300

    Modified project build script

--- PCIe_5GRadio_lms7_trx.tcl
+++ PCIe_5GRadio_lms7_trx.tcl
@@ -168,7 +168,7 @@
 #*****************************************************************************************
 
 # Set the reference directory for source file relative paths (by default the value is script directory path)
-set origin_dir "."
+set origin_dir [file dirname [info script]]
 
 # Use origin directory path location variable, if specified in the tcl shell
 if { [info exists ::origin_dir_loc] } {
