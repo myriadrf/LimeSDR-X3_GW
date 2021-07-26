77bb44e928dbda696fb505e5cdd7783eb6d3b0fa
servenikas <tomas.s@limemicro.lt>
2021-07-26 17:01:13 +0300

    Updated gen_prog_file script to be portable

--- gen_prog_file.tcl
+++ gen_prog_file.tcl
@@ -1,1 +1,12 @@
-write_cfgmem  -format mcs -force -size 16 -interface SPIx4 -loadbit {up 0x00000000 "/home/servenikas/git/pcie_5gradio_gw/PCIe_5GRadio_lms7_trx/PCIe_5GRadio_lms7_trx.runs/impl_1/lms7_trx_top.bit" } -file "/home/servenikas/git/pcie_5gradio_gw/bitstream/lms7_trx.mcs"
+# Set the reference directory to where the script is
+set script_path [file dirname [file normalize [info script]]]
+puts $script_path
+
+# Set output file path and name
+set bit_path "[file normalize "$script_path/PCIe_5GRadio_lms7_trx/PCIe_5GRadio_lms7_trx.runs/impl_1/lms7_trx_top.bit"]"
+puts $bit_path
+
+set bit_string "up 0x00000000 $bit_path "
+puts $bit_string
+
+write_cfgmem  -format mcs -force -size 16 -interface SPIx4 -loadbit $bit_string -file "[file normalize "$script_path/bitstream/lms7_trx.mcs"]"
